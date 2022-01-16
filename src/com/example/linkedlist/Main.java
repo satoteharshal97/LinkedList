d4cb8d3405c67eadbe93a98b6b392b23441aa616
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-16 11:54:53 +0530

    Added- Here 70 is created first and then 30 and then 56

--- src/com/example/linkedlist/Main.java
+++ src/com/example/linkedlist/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList<Integer> linkedList = new LinkedList<Integer>();
-        MyNode <Integer> firstNode = new MyNode<>(56);
+        MyNode <Integer> firstNode = new MyNode<>(70);
         MyNode <Integer> secondNode = new MyNode<>(30);
-        MyNode <Integer> thirdNode = new MyNode<>(70);
+        MyNode <Integer> thirdNode = new MyNode<>(56);
         firstNode.setNext(secondNode);
         secondNode.setNext(thirdNode);
         System.out.println(firstNode);
